57d03b641b4109388f30eb12bd177105faf6277f
mtekateka <39678622+mtekateka@users.noreply.github.com>
2018-08-07 01:06:47 +0300

    Update Hello.py

--- 1.Basic Fundamental/Hello.py
+++ 1.Basic Fundamental/Hello.py
@@ -6,6 +6,19 @@ In every language we like to Salute to the World by saying Hello World.
 print('Hello World')
 
 #To comment in Python we use hash 
-print('To comment on Python we use # sign for the Single line of code') 
-
+'''
+eg #this is a comment...
+'''
+#Print Statements available in python..
+'''
+The print statement can be used in the following different ways :
+'''
+print(variable_name)
+print("Hello World")
+print ("Hello", <Variable >)
+print ("Hello" + <Variable Containing the String>)
+print ("Hello %s" % <variable containing the string>)
+print ("Hello" + <Variable Containing the String>)
+print ("Hello %d" % <variable containing integer datatype>)
+print ("Hello %f" % <variable containing float datatype>)
 
